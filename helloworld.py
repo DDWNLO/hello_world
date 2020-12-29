f6e75d6b2f0eee12ce3c19373b988cdf59508fe2
DDWNLO <dennis.de.wekker@nederlandseloterij.nl>
2020-12-29 15:22:02 +0100

    Code oefeningen toegevoegd

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,14 @@
+import pandas as pd 
+
+#Set vars
+name='Dennis'
+number=10
+
+#Cast the int to string
+str_number=str(number)
+
+#Concat to 1 string
+string=name+'_'+str_number
+
 #commentaar
-print('Hello world.')
+print(string)
